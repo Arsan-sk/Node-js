36e9b74c8e71ffd49a754b7627c9b7bee0eb0bf1
Your Name <your.email@example.com>
2025-10-28 19:24:18 +0530

    understande CLI based configuring tailwind with node js and implement it as well

---
+++ tailwind-ejs/app.js
@@ -0,0 +1,36 @@
+// external imports
+const express = require('express');
+
+// routers/local imports
+const userRouter = require('./routes/userRoutes');
+const hostRouter = require('./routes/hostRoutes');
+const rootDir = require('./utils/pathUtils');
+
+//core imports
+const path = require('path');
+
+const app = express();
+
+// middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// serve static assets (CSS, JS, images) from the public folder
+app.use(express.static(path.join(rootDir, 'public')));
+
+// using routers as middlewares
+app.use(userRouter);
+app.use("/host",hostRouter);
+
+
+
+app.use((req, res, next) => {
+   res.status(404).sendFile(path.join(rootDir, 'views', 'pageNotFound.html'));
+   console.log(req.url, req.method)
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
